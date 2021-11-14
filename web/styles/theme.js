71abae6e5914a00df96489fb492bb46c24f515bf
Filip Cordas <cordas@noisolation.com>
2021-11-14 17:32:39 +0100

    feat: new theme

---
+++ web/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    white: "#FFF",
+    black: "#000",
+    greenDark: "#049648",
+    neutralGray: "#F5F5F5",
+    purpleDark: "#4E134C",
+    purpleLight: "#91288C",
+    blueLight: "#2A3B8F",
+  },
+  gradients: {
+    orange: ["#F27323", "#F9A61A"],
+    greenYellow: ["#049648", "#8DC343", "#FCBB16", "#91288C"],
+    green: ["#049648", "#8DC343"],
+  },
+};
+
+export default theme;
