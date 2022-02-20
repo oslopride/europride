a96ce66190cbc4e110705beac87071eb66221a1f
Filip Cordas <cordas@noisolation.com>
2022-02-20 15:25:53 +0100

    feat: header@partner types on front and cleanup

---
+++ web/src/components/PartnerBox/index.tsx
@@ -0,0 +1,111 @@
+import styled from "@emotion/styled";
+import CreateSanityImage from "../../components/CreateSanityImage";
+import { StyledProps } from "../../types/theme";
+
+interface ISizes {
+  [key: string]: number;
+}
+
+const SIZES: ISizes = {
+  owner: 250,
+  main: 200,
+  regular: 150,
+};
+
+const PartnerBox = ({ partners, config }: any) => {
+  const partnerTypes = ["owner", "main", "regular"];
+
+  const getWidth = (type: string) => SIZES[type];
+
+  const createPartners = (partners: any, type: string) => {
+    return (
+      <PartnerItem key={type}>
+        <PartnerTypeHeader>{config[type].eng}</PartnerTypeHeader>
+        <PartnerRow>
+          {partners
+            .filter((partner: any) => partner.type === type)
+            .map((partner: any) => {
+              return (
+                <PartnerImageWrapper
+                  width={getWidth(partner.type)}
+                  key={partner.name + partner.slug.current}
+                >
+                  <CreateSanityImage
+                    url={partner?.image.asset}
+                    alt={partner?.name}
+                  />
+                </PartnerImageWrapper>
+              );
+            })}
+        </PartnerRow>
+      </PartnerItem>
+    );
+  };
+  return (
+    <PartnerContainer>
+      <PartnerHeader>{config.header.eng}</PartnerHeader>
+      <PartnerImagesWrapper>
+        {partnerTypes.map((type) => createPartners(partners, type))}
+      </PartnerImagesWrapper>
+    </PartnerContainer>
+  );
+};
+
+export default PartnerBox;
+
+const PartnerContainer = styled.div`
+  flex: 1;
+  flex-direction: column;
+  background: #f5f5f5;
+  padding: 30px;
+  @media (min-width: ${({ theme }: StyledProps) =>
+    theme?.breakpoints?.tablet}px) {
+    padding: 63px;
+  }
+}
+`;
+
+const PartnerImagesWrapper = styled.div`
+  margin-top: 30px;
+`;
+
+const PartnerHeader = styled.p`
+  font-size: 24px;
+  font-weight: 800;
+  align-items: center;
+  max-width: 1440px;
+`;
+
+const PartnerTypeHeader = styled.p`
+  font-size: 18px;
+  font-weight: 800;
+  align-self: center;
+`;
+
+const PartnerItem = styled.article`
+  margin-bottom: 20px;
+  border-bottom: 1px solid rgba(70, 70, 70, 0.5);
+  &:last-of-type {
+    border-bottom: none;
+    margin-bottom: 0px;
+  }
+`;
+
+const PartnerRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  padding: 20px 0;
+  @media (min-width: ${({ theme }: StyledProps) =>
+      theme?.breakpoints?.tablet}px) {
+    flex-direction: row;
+  }
+`;
+
+const PartnerImageWrapper = styled.div<{ width: number }>`
+  flex: 1;
+  max-width: ${({ width }) => width}px;
+  min-width: 150px;
+  max-height: 150px;
+  margin-right: 10px;
+`;
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,45 +2,9 @@ import type { NextPage } from "next";
 import styled from "@emotion/styled";
 import configuredSanityClient from "../sanity";
 import CreateSanityImage from "../components/CreateSanityImage";
-import SanityBlock from "../components/SanityBlock";
 import { StyledProps } from "../types/theme";
-
-interface ISizes {
-  [key: string]: number;
-}
-
-const getWidth = (type: string) => {
-  const SIZES: ISizes = {
-    owner: 250,
-    main: 200,
-    regular: 150,
-  };
-  return SIZES[type];
-};
-
-const createPartners = (partners: any, type: string) => {
-  return (
-    <PartnerItem>
-      <PartnerRow>
-        {partners
-          .filter((partner: any) => partner.type === type)
-          .map((partner: any) => {
-            return (
-              <PartnerImageWrapper
-                width={getWidth(partner.type)}
-                key={partner.name + partner.slug.current}
-              >
-                <CreateSanityImage
-                  url={partner?.image.asset}
-                  alt={partner?.name}
-                />
-              </PartnerImageWrapper>
-            );
-          })}
-      </PartnerRow>
-    </PartnerItem>
-  );
-};
+import SanityBlock from "../components/SanityBlock";
+import PartnerBox from "../components/PartnerBox";
 
 const Home: NextPage = ({ data, partners, config }: any) => {
   return (
@@ -58,14 +22,7 @@ const Home: NextPage = ({ data, partners, config }: any) => {
           <SanityBlock blocks={data?.body.eng} />
         </BlockWrapper>
       </Wrapper>
-      <PartnerContainer>
-        <PartnerHeader>{data?.header?.partnerHeader?.eng}</PartnerHeader>
-        <PartnerImagesWrapper>
-          {createPartners(partners, "owner")}
-          {createPartners(partners, "main")}
-          {createPartners(partners, "regular")}
-        </PartnerImagesWrapper>
-      </PartnerContainer>
+      <PartnerBox partners={partners} config={config} />
     </>
   );
 };
@@ -74,7 +31,7 @@ export const getServerSideProps = async () => {
   const data = await configuredSanityClient.fetch(`*[_type == "frontPage"][0]`);
   const partners = await configuredSanityClient.fetch(`*[_type == "partner"]`);
   const config = await configuredSanityClient.fetch(
-    `*[_type == "webConfiguration"]`
+    `*[_type == "webConfiguration"][0].partners`
   );
   if (!data) {
     return {
@@ -150,55 +107,6 @@ const HeroImageWrapper = styled.div`
   width: 100%;
   max-width: 1280px;
   align-self: center;
-`;
-
-const PartnerContainer = styled.div`
-  flex: 1;
-  flex-direction: column;
-  background: #f5f5f5;
-  padding: 30px;
-  @media (min-width: ${({ theme }: StyledProps) =>
-    theme?.breakpoints?.tablet}px) {
-    padding: 63px;
-  }
-}
-`;
-
-const PartnerItem = styled.article`
-  margin-bottom: 20px;
-  border-bottom: 1px solid rgba(70, 70, 70, 0.5);
-  &:last-of-type {
-    border-bottom: none;
-    margin-bottom: 0px;
-  }
-`;
-
-const PartnerRow = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  padding: 20px 0;
-  @media (min-width: ${({ theme }: StyledProps) =>
-      theme?.breakpoints?.tablet}px) {
-    flex-direction: row;
-  }
-`;
-
-const PartnerHeader = styled.p`
-  font-size: 24px;
-  font-weight: 800;
-  align-items: center;
-  max-width: 1440px;
-`;
-
-const PartnerImagesWrapper = styled.div``;
-
-const PartnerImageWrapper = styled.div<{ width: number }>`
-  flex: 1;
-  max-width: ${({ width }) => width}px;
-  min-width: 150px;
-  max-height: 150px;
-  margin-right: 10px;
 `;
 
 export default Home;
